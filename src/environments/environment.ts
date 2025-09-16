38d7d7a4048868dd275e85270dec5df4bf064df7
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-15 20:01:30 -0500

    feat(environments): add environment variables support (for illustration purposes only).

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  newsProviderApiBaseUrl: 'https://newsapi.org/v2',
+  newsProviderNewsEndpointPath: '/top-headlines',
+  newsProviderSourcesEndpointPath: '/top-headlines/sources',
+  newsProviderApiKey: '',
+  logoProviderApiBaseUrl: 'https://logo.clearbit.com'
+};
