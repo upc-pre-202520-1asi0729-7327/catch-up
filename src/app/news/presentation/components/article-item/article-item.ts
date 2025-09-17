47f19ae3e3beb3e8f20c4f344b79d005ca642b9f
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-17 17:23:18 -0500

    feat(news): add article item component.

---
+++ src/app/news/presentation/components/article-item/article-item.ts
@@ -0,0 +1,54 @@
+import {Component, inject, input} from '@angular/core';
+import {
+  MatCard, MatCardActions,
+  MatCardContent,
+  MatCardHeader, MatCardImage,
+  MatCardSubtitle,
+  MatCardTitle,
+  MatCardTitleGroup
+} from '@angular/material/card';
+import {DatePipe} from '@angular/common';
+import {MatButton, MatIconButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
+import {TranslatePipe} from '@ngx-translate/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {Article} from "../../../domain/model/article.entity";
+import {catchError} from 'rxjs';
+
+@Component({
+  selector: 'app-article-item',
+  imports: [MatCard, MatCardHeader, MatCardTitleGroup, MatCardTitle, MatCardSubtitle, DatePipe, MatCardContent, MatCardActions, MatButton, MatCardImage, TranslatePipe, MatIconButton, MatIcon],
+  templateUrl: './article-item.html',
+  styleUrl: './article-item.css'
+})
+export class ArticleItem {
+  private snackBar = inject(MatSnackBar);
+  article = input.required<Article>();
+
+  async shareArticle() {
+    const articleShareInfo = {
+      title: this.article()?.title,
+      url: this.article()?.url,
+    };
+
+    if (navigator.share) {
+      try {
+        await navigator.share(articleShareInfo);
+        this.snackBar.open('Article shared successfully!', 'Close', {duration: 3000});
+      } catch (error) {
+        this.snackBar.open('Sharing failed', 'Close', {duration: 3000});
+      }
+    } else {
+      try {
+        if (articleShareInfo.url) {
+          await navigator.clipboard.writeText(articleShareInfo.url);
+          this.snackBar.open('Article URL copied to clipboard!', 'Close', {duration: 3000});
+        }
+      } catch (error) {
+        this.snackBar.open('Failed to copy URL', 'Close', {duration: 3000});
+      }
+    }
+  }
+
+
+}
