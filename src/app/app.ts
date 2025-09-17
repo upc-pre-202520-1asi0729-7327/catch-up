8282c9004b991b9deefec27e988cc865edb74107
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-17 17:58:05 -0500

    feat(layout): integrate layout component into main view.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import {Layout} from './shared/presentation/components/layout/layout';
 
 @Component({
